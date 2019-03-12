729fde838b231348f9c7e57f2499fd49487ed6f4
polleichtnermoritz <moritz.polleichtner@outlook.com>
2019-03-12 11:24:27 +0100

    done

--- bst.cpp
+++ bst.cpp
@@ -1,179 +1,113 @@
-/*-----------------------------------------------------------------------------
- *				HTBLA-Leonding / Class: <your class name here>
- *-----------------------------------------------------------------------------
- * Exercise Number: #exercise_number#
- * File:			bst.cpp
- * Author(s):		Peter Bauer
- * Due Date:		May 31, 2017
- *-----------------------------------------------------------------------------
- * Description:
- * <your description here>
- *-----------------------------------------------------------------------------
-*/
+#include <stdio.h>
 #include "bst.h"
-#include "general.h"
 #include <stdlib.h>
+#include "general.h"
+#define MAXL 50
 
-struct Node
-{
-  int element;
-  struct Node* left;
-  struct Node* right;
+struct Node{
+  int data;
+   Bst left_subtree;
+   Bst right_subtree;
 };
 
-
-Bst new_bst()
-{
-  Bst bst = (Bst)malloc(sizeof(struct Node));
-  bst=0;
-
-  return bst;
+Bst new_bst(){
+   Bst _head = 0;
+   return _head;
+}
+void delete_bst(Bst bst){
+  if(bst == 0)return;
+  delete_bst(bst->left_subtree);
+  delete_bst(bst->right_subtree);
+  sfree(bst);
 }
 
-void delete_bst(Bst bst)
-{
-  if (bst != 0)
-  {
-    delete_bst(bst->left);
-    delete_bst(bst->right);
-    sfree(bst);
+int get_depth(Bst bst){
+  if(bst == 0)return 0;
+  int depth = get_depth(bst->left_subtree)+1;
+  int r = get_depth(bst->right_subtree)+1;
+  return  r>depth ? r:depth;
+}
+
+void add(Bst* bst, int value){
+  Bst &aka = *bst;
+  if(aka == 0){
+    aka = (Bst)malloc(sizeof(struct Node));
+    aka->data = value;
+    aka->left_subtree =0;
+    aka->right_subtree =0;
+  }
+  else if(value <= aka->data){
+      add(&aka->left_subtree, value);
+  }
+  else{
+      add(&aka->right_subtree,value);
   }
 }
-/**
-*** @return The depth of the BST
-*/
-int get_depth(Bst bst)
-{
-  if (bst == 0)
-  {
-    return 0;
+
+int root_value(Bst bst){
+  return bst==0?0:bst->data;
+}
+
+Bst left_subtree(Bst root){
+  return root == 0?0:root->left_subtree;
+}
+
+Bst right_subtree(Bst root){
+  return root == 0?0:root->right_subtree;
+}
+
+int traverse_pre_order(Bst bst, int *elements, int start){
+    if(bst == 0)return 0;
+    int count = 0;
+    elements[start] = bst->data;
+    count += traverse_pre_order(bst->left_subtree, elements, start+1);
+    count+= traverse_pre_order(bst->right_subtree, elements, start+count+1);
+    return count+1;
+}
+
+int traverse_in_order(Bst bst, int *elements, int start){
+    if(bst == 0)return 0;
+    int count = 0;
+    count += traverse_in_order(bst->left_subtree, elements, start);
+    elements[start+count] = bst->data;
+    count+= traverse_in_order(bst->right_subtree, elements, start+count+1);
+    return count+1;
+}
+
+int traverse_post_order(Bst bst, int *elements, int start){
+    if(bst == 0)return 0;
+    int count = 0;
+    count += traverse_post_order(bst->left_subtree, elements, start);
+    count += traverse_post_order(bst->right_subtree, elements, start+count);
+    elements[start+count] = bst->data;
+    return count+1;
+}
+
+bool are_equal(Bst bst1, Bst bst2){
+  if(bst1 == bst2)return true;
+  if(bst1 != 0 && bst2 != 0){
+    int arr1[MAXL];
+    int length = traverse_pre_order(bst1, arr1, 0);
+    int arr2[MAXL];
+    int length2 = traverse_pre_order(bst2, arr2, 0);
+    if(length == length2){
+      bool x = true;
+      for (int i = 0; i < length; i++) {
+        if(arr1[i]!= arr2[i])x = false;
+      }
+      return x;
+    }
   }
-  int counter = 0;
-  if (bst->right != 0)
-  {
-    counter = get_depth(bst->right);
-  }
-  if (bst->left != 0)
-  {
-    counter = get_depth(bst->left);
-  }
-  return counter;
+  return false;
 }
 
-/**
-*** Adds a value to the BST
-*/
-void add(int value, Bst *bst)
-{
-  if (*bst == 0)
-  {
-    Bst node = (Bst)malloc(sizeof(struct Node));
-    node->left = 0;
-    node->right = 0;
-    node->element = value;
-    *bst = node;
-  }
-  else if (value <= (*bst)->element)
-  {
-    add(&(*bst)->left, value);
-  }
-  else
-  {
-      add(&(*bst)->right, value);
-  }
-}
-/**
-*** @return The value of the root element of the BST
-*/
-int root_value(Bst bst)
-{
-  return bst->element;
+void most_left_longest_branch(Bst bst, Bst* branch){
+    if(bst == 0)return;
+    add(branch, bst->data);
+    get_depth(bst->left_subtree) >= get_depth(bst->right_subtree) ? most_left_longest_branch(bst->left_subtree, branch):most_left_longest_branch(bst->right_subtree, branch);
 }
 
-/**
-*** @return The left subtree of the BST
-*/
-Bst left_subtree(Bst root)
-{
-  return root->left;
-}
-
-/**
-*** @return The right subtree of the BST
-*/
-Bst right_subtree(Bst root)
-{
-  return root->right;
-}
-
-/**
-*** Traverses the BST depth first by first returning the root, then traversing the left
-*** subtree, then traversing the right subtree.
-*** @param bst The BST to be traversed
-*** @param elements Array of elements in the order how they are found during traversal
-*** @param start Start index of elements wherer the function should start to add the found elements
-*** @return Number of elements found during traversal
-*/
-int traverse_pre_order(Bst bst, int *elements, int start)
-{
-  return 0;
-}
-
-/**
-*** Traverses the BST depth first by first traversing the left subtree, then adding the root,
-*** then traversing the right subtree.
-*** @param bst The BST to be traversed
-*** @param elements Array of elements in the order how they are found during traversal
-*** @param start Start index of elements wherer the function should start to add the found elements
-*** @return Number of elements found during traversal
-*/
-int traverse_in_order(Bst bst, int *elements, int start)
-{
-  return 0;
-}
-
-/**
-*** Traverses the BST depth first by first traversing the left subtree, then traversing the right
-*** subtree and finally adding the root.
-*** @param bst The BST to be traversed
-*** @param elements Array of elements in the order how they are found during traversal
-*** @param start Start index of elements wherer the function should start to add the found elements
-*** @return Number of elements found during traversal
-*/
-int traverse_post_order(Bst bst, int *elements, int start)
-{
-  return 0;
-}
-
-/**
-*** Checks whether two trees are equal
-*** @param bst1 First bst
-*** @param bst2 Second bst
-*** @return true if bst1 and bst2 are equal, false otherwise
-*/
-bool are_equal(Bst bst1, Bst bst2)
-{
-  return 0;
-}
-
-/**
-*** Calculates the longest branch in the tree. If two branches are equally long
-*** the left branch is taken.
-*** @param bst Bst which longest branch should be found
-*** @return A tree where each node has only one child equal to the longest
-*** branch of bst
-*/
-void most_left_longest_branch(Bst bst, Bst* branch)
-{
-
-}
-
-/**
-*** Gets the number of non-empty subtrees of a tree
-*** @param bst The tree which number of subtrees to get
-*** @return The number of non-empty subtrees
-*/
-int get_number_of_subtrees(Bst bst)
-{
-  return 0;
+int get_number_of_subtrees(Bst bst){
+    if(bst == 0)return -1;
+return get_number_of_subtrees(bst->left_subtree) +1+ get_number_of_subtrees(bst->right_subtree)+1;
 }
